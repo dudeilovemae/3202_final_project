cdde58f34dd43a1701c1914f7fb6c4b3d219d8cf
dudeilovemae <rdrfranco@gmail.com>
2025-11-27 17:38:53 -0700

    created a version to record the episodes

---
+++ scripts/lunarLander_random_record.py
@@ -0,0 +1,50 @@
+import gymnasium as gym
+from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
+
+# Initialise the Lunar Lander Environment 
+env = gym.make("LunarLander-v3", render_mode="rgb_array")
+num_of_episodes = 4
+folder="lunarLander_random"
+
+
+# install env wrapper to record a video
+env = RecordVideo(
+    env,
+    video_folder=folder,                  # Folder
+    name_prefix="rand",                   # Video filename prefix
+    episode_trigger=lambda x: True        # Record every episode
+)
+
+print(f"Running {num_of_episodes} episodes...")
+print(f"Saving vides to: {folder}")
+
+# Repeat for set number of episodes
+for episode_count in range(num_of_episodes):
+    # We need to reset the environment before first use
+    observation, info = env.reset()
+
+    # initialize total score to zero and step count
+    total_score = 0
+    num_of_steps = 0
+    success_criteria = None
+
+    episode_over = False
+
+    while not episode_over:
+        # For now we will use a random agent
+        action = env.action_space.sample()
+
+        # Take the random action and recieve information 
+        observation, reward, terminated, truncated, info = env.step(action)
+        total_score = total_score + reward
+
+        num_of_steps += 1
+
+        episode_over = terminated or truncated
+
+    # The documentation states that a score of 200 or more is a solution
+    success_criteria = reward >= 200
+    print(f"Episode {episode_over + 1}: {num_of_episodes} steps, reward = {total_score}, success: {success_criteria} ")
+
+
+env.close()
