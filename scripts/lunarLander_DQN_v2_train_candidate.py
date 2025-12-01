da8b50789775de05fa7ee6a4582ec9739ac71912
dudeilovemae <rdrfranco@gmail.com>
2025-11-30 23:06:49 -0700

    this was just a backup

---
+++ scripts/lunarLander_DQN_v2_train_candidate.py
@@ -0,0 +1,42 @@
+# Had to revert to gym for sb3
+import gym 
+from stable_baselines3 import DQN
+import stable_baselines3.common.evaluation as eval
+
+# Init the lunar lander env
+# had to revert to v2 for sb3
+env = gym.make("LunarLander-v2")
+
+
+model = DQN("MlpPolicy", 
+            env, 
+            verbose=0, # no logs
+            exploration_fraction=0.4, # explore longer
+            exploration_final_eps=0.1, # Keep some random at the end
+            gamma=0.99,
+            learning_starts=30000, # Dont start learning until the buffer is fuller
+            buffer_size=250000, # increasing buffer size
+            batch_size=32*4, # four times the default batch size
+            target_update_interval=500,
+            tensorboard_log="logs/dqn_v2_tensorboard_log")
+
+print(f"Calling learn for 800_000 timesteps...")
+model.learn(total_timesteps=800_000)
+model.save("models/dqn_lunarlander_v2")
+print(f"Learn complete. ")
+
+print(f"Now evaluating policy")
+avg_reward, std_reward = eval.evaluate_policy(model, env, n_eval_episodes=25, deterministic=True)
+
+print(f"\nEvaluation has shown: \nAverage Reward: {avg_reward}\nStandard Dev: {std_reward}")
+
+obs = env.reset()
+
+for _ in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+        obs = env.reset()
+
+env.close()
